1dfef50c4ebd4ebbebfad100323bf2a51bdf671e
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2026-07-08 18:22:23 +0200

    docs: revert README documentation changes

--- commands/test.py
+++
@@ -1,6 +0,0 @@
-import discord
-from discord import app_commands
-
-@app_commands.command(name="test", description="Test if the bot is running")
-async def test(interaction: discord.Interaction):
-    await interaction.response.send_message("Bot is working", ephemeral=True)
--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
